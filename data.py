75e30c92360c6de2938677e4a4df7e4ac7233b2b
Mert Kahyaoğlu <mertkahyaoglu93@gmail.com>
2014-12-02 17:29:09 +0200

    webpy

---
+++ data.py
@@ -0,0 +1,102 @@
+smartphones = [
+    {
+        "img": "/static/img/item.jpg",
+        "brand": "Apple",
+        "model": "iPhone 6",
+        "display": "4.7\" IPS Display",
+        "chipset": "A8 Dual-core 1.4 Ghz",
+        "camera": "8.0MP Isight Camera",
+        "battery": "1850Mah Battery",
+        "content": "This is Apple like we've never seen before. Apple like we never thought we'll see.",
+        "featured": True,
+        "featuredImage": "/static/img/3.jpg",
+        "popular": True,
+        "images": [
+            "/static/img/ip6-item-detail.jpg",
+            "/static/img/ip6-item-detail2.jpg",
+            "/static/img/ip6-item-detail3.jpg"
+        ]
+    },
+    {   
+        "img": "/static/img/item2.jpg",
+        "brand": "Samsung",
+        "model": "Note 4",
+        "display": "5.7\" IPS Display",
+        "chipset": "Qualcomm Snapdragon Quad-core 2.7 Ghz",
+        "camera": "16.0MP Camera",
+        "battery": "3220Mah Battery",
+        "content": "From an oddball that everyone was laughing at to becoming one of the most important Samsung announcement of the year - the Note 4!",
+        "featured": True,
+        "featuredImage": "/static/img/2.jpg",
+        "popular": True,
+        "images": [
+            "/static/img/note4-item-detail.jpg",
+            "/static/img/note4-item-detail2.jpg",
+            "/static/img/note4-item-detail3.jpg"
+        ]
+    },
+    {   
+        "img": "/static/img/item3.jpg",
+        "brand": "Sony",
+        "model": "Z3",
+        "display": "5.2\" IPS Display",
+        "chipset": "Qualcomm Snapdragon Quad-core 2.5 Ghz",
+        "camera": "21.0MP Camera",
+        "battery": "3200Mah Battery",
+        "content": "To say we are eager to get the Xperia Z3 out for a spin would be a massive understatement",
+        "featured": True,
+        "featuredImage": "/static/img/4.jpg",
+        "images": [
+            "/static/img/z3-item-detail.jpg",
+            "/static/img/z3-item-detail2.jpg",
+            "/static/img/z3-item-detail3.jpg"
+        ]
+    },
+    {   
+        "img": "/static/img/item4.jpg",
+        "brand": "LG",
+        "model": "G3",
+        "display": "5.5\" IPS Display",
+        "chipset": "Qualcomm Snapdragon Quad-core 2.3 Ghz",
+        "camera": "13.0MP Camera",
+        "battery": "3000Mah Battery",
+        "content": "Late as usual but ever so strong.",
+        "images": [
+            "/static/img/g3-item-detail.jpg",
+            "/static/img/g3-item-detail2.jpg",
+            "/static/img/g3-item-detail3.jpg"
+        ]
+    },
+    {   
+        "img": "/static/img/item5.jpg",
+        "brand": "Google",
+        "model": "Nexus 6",
+        "display": "5.9\" IPS Display",
+        "chipset": "Qualcomm Snapdragon Quad-core 2.7 Ghz",
+        "camera": "13.0MP Camera",
+        "battery": "3000Mah Battery",
+        "content": "Nexus 6 by Motorola marks a shift in market positioning for Google's smartphone lineup.",
+        "featured": True,
+        "featuredImage": "/static/img/1.jpg",
+        "images": [
+            "/static/img/nexus6-item-detail.jpg",
+            "/static/img/nexus6-item-detail2.jpg",
+            "/static/img/nexus6-item-detail3.jpg"
+        ]
+    }
+]
+
+reviews = [
+    {
+        "img": "/static/img/review1.jpg",
+        "title" : "Motorola Nexus 6 Review",
+        "permalink": "motorola-nexus-6-review",
+        "review" : "Nexus 6 by Motorola marks a shift in market positioning for Google's smartphone lineup. Whereas the duo of LG-made devices that precede it cut back on certain features to stay attractively"
+    },
+    {
+        "img": "/static/img/review2.jpg",
+        "title": "Iphone 6 Plus vs Note 4",
+        "permalink": "iphone6-plus-vs-note-4",
+        "review" : "Samsung's release cadence dictates that the Galaxy Note is the H2 flagship, keen to assert its superiority over an already feature-rich Galaxy S. iPhones come once a year, even the first-time iPhone 6 Plus phablet."
+    }
+]
