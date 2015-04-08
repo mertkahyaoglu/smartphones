7f5119c93e1baf15d2ce1d2f1d41dd5abbf567f9
Mert Kahyaoğlu <mertkahyaoglu93@gmail.com>
2015-04-09 01:09:28 +0300

    update port

--- server.py
+++ server.py
@@ -142,5 +142,4 @@ class populars:
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
